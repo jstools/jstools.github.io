d694cca838c797a3c9d0de017ab0e9c0edd9796c
Jesús Germade <jesus@germade.es>
2015-08-07 08:39:36 +0200

    updating libraries and using history.pushState

--- assets/jstools.js
+++ assets/jstools.js
@@ -5,6 +5,44 @@
     }
   } );
 
+  (function () {
+
+    var RE_origin = new RegExp('^' + location.origin.replace(/\\/, '\\').replace(/\//, '\\\/') ),
+        jBody = $(document.body);
+
+    $.plugin('a', function (jLinks) {
+      jLinks.click(function (e) {
+        var link = this;
+
+        if( RE_origin.test(link.href) ) {
+
+            e.preventDefault();
+
+            $('article.article').addClass('is-loading');
+            $http(link.href).get()
+              .then(function (response) {
+                var bodyHTML = response.data.split(/\<\/?body[^>]*\>/)[1];
+                jBody.html(bodyHTML);
+                history.pushState({
+                  body: bodyHTML
+                }, 'jsTools', link.href.replace(RE_origin, '') );
+              })
+              .finally(function () {
+                $('article.article').removeClass('is-loading');
+              });
+        }
+      });
+    }, true);
+
+    $(window).on('popstate', function(event) {
+      if( event.state && event.state.body ) {
+        jBody.html(event.state.body);
+      }
+    });
+
+  })();
+
+
 
   (function () {
 
@@ -14,22 +52,29 @@
       jBody.removeClass('show-nav');
     });
 
-    $('#libs-nav').click(function (e) {
-      e.stopPropagation();
-    });
+    // $.plugin('#libs-nav', function (jLinks) {
+    //   jLinks.click(function (e) {
+    //     e.stopPropagation();
+    //   });
+    // }, true);
 
-    $('#header .logo-link').click(function (e) {
+    $.plugin('#header .logo-link', function (jLogoLink) {
 
-      if (window.matchMedia("(max-width: 768px)").matches) {
-        e.stopPropagation();
-        e.preventDefault();
+      jLogoLink.click(function (e) {
 
-        if( jBody.hasClass('show-nav') ) {
-          jBody.removeClass('show-nav');
-        } else {
-          jBody.addClass('show-nav');
+        if (window.matchMedia("(max-width: 768px)").matches) {
+          e.stopPropagation();
+          e.preventDefault();
+
+          if( jBody.hasClass('show-nav') ) {
+            jBody.removeClass('show-nav');
+          } else {
+            jBody.addClass('show-nav');
+          }
         }
-      }
-    });
+      });
+
+    }, true);
+
 
   })();
